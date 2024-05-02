769b6915318c033f605b42fa016b5ae12e944aa8
K_DM <k.datamining@gmail.com>
2024-05-03 00:20:15 +0900

    [update] app

--- pages/[1] 線形回帰.py
+++ pages/[1] 線形回帰.py
@@ -1,6 +1,7 @@
 import japanize_matplotlib as jam
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import streamlit as st
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
@@ -28,7 +29,7 @@ def _do_residual_analysis(
 
     st.markdown("## 各変数と残差の比較")
     fig = plt.figure(figsize=(8, 8))
-    for i in range(1, 10):
+    for i in range(1, X.shape[1] + 1):
         plt.subplot(3, 3, i)
         plt.scatter(X[:, i - 1], y - pred, s=1)
         plt.xlabel(f"x{i}")
@@ -54,22 +55,52 @@ def _do_residual_analysis(
     return mae, r2
 
 
+@st.cache_data
+def get_sample_data(n, pattern=0, allpositive=False, add_dummy=False):
+    data = []
+
+    for i in range(n):
+        x1 = np.random.randint(10, 100)
+        if allpositive:
+            x2 = np.random.randint(10, 40)
+        else:
+            x2 = np.random.randint(-10, 20)
+        x3 = np.random.randint(100, 200)
+
+        y = (
+            2.3 * (x1 + np.random.rand())
+            - 10 * (x2 + np.random.rand())
+            + 0.1 * (x3 + np.random.rand())
+            + np.random.rand() * 10
+        )
+
+        if add_dummy:
+            x4 = np.random.randint(-10, 10)
+            x5 = np.random.randint(-200, -100)
+            y += x4 * x1
+            y += x5
+
+        data.append([x1, x2, x3, y])
+
+    df = pd.DataFrame(data)
+    df.columns = ["x1", "x2", "x3", "y"]
+    return df[["x1", "x2", "x3"]].values, df["y"].values
+
+
+st.markdown("# 線形回帰")
+
 N = int(st.slider("プロットするデータ数", 100, 1000, 500))
 
 tab1, tab2, tab3, tab4 = st.tabs(["線形回帰が適切な例", "外れ値がある例", "非線形な例", "説明変数がXに含まれていない時"])
 
 with tab1:
-    X, y = make_regression(
-        n_samples=N, n_features=9, noise=10, random_state=777, tail_strength=0.0
-    )
+    X, y = get_sample_data(N)
     _do_residual_analysis(X, y, model=LinearRegression())
 
 with tab2:
     anom_N = int(st.slider("はずれ値の数", 1, int(N / 10), 20))
     st.markdown("## はずれ値を除外しない場合")
-    X, y = make_regression(
-        n_samples=N, n_features=9, noise=10, random_state=777, bias=1000
-    )
+    X, y = get_sample_data(N)
     for i in range(anom_N):
         y[i] *= 4
     mae, r2 = _do_residual_analysis(X, y, model=LinearRegression())
@@ -79,18 +110,27 @@ with tab2:
     )
 
 with tab3:
-    X, y = make_regression(
-        n_samples=N, n_features=9, noise=10, random_state=777, bias=1000
-    )
-    y += 10
-    y = y**3
+    st.markdown("## 非線形の関係がある場合")
+    X, y = get_sample_data(N, allpositive=True)
+    X[:, 0] = X[:, 0] ** 2
+    X[:, 1] = X[:, 1] ** 2
+    X[:, 2] = X[:, 2] ** 2
 
-    _do_residual_analysis(X, y, model=LinearRegression())
+    mae, r2 = _do_residual_analysis(X, y, model=LinearRegression())
+
+    st.markdown("## 対数変換した場合")
+    options = st.multiselect("対数変換する変数を選んでください", ["x1", "x2", "x3"], ["x1", "x2", "x3"])
+
+    if "x1" in options:
+        X[:, 0] = np.sqrt(X[:, 0])
+    if "x2" in options:
+        X[:, 1] = np.sqrt(X[:, 1])
+    if "x3" in options:
+        X[:, 2] = np.sqrt(X[:, 2])
+
+    _do_residual_analysis(X, y, model=LinearRegression(), pmae=mae, pr2=r2)
 
 with tab4:
-    X, y = make_regression(
-        n_samples=N, n_features=12, noise=10, random_state=777, tail_strength=0.0
-    )
+    X, y = get_sample_data(N, allpositive=True, add_dummy=True)
 
-    y = X[:, 10] + X[:, 11]
-    _do_residual_analysis(X[:, :9], y, model=LinearRegression())
+    _do_residual_analysis(X[:, :3], y, model=LinearRegression())
---
+++ トップページ.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown(
+    """
+- 線形回帰：線形回帰モデルの作成例
+- 回帰診断：回帰診断の実行例
+"""
+)
